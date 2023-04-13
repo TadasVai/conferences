14c01ef14de0871db0045178a8c4aef9062bd7ee
TadasVai <tadas.vaiciulenas@stud.vilniustech.lt>
2023-04-13 17:30:24 +0300

    working with blade v1.1

---
+++ resources/views/conferences/show.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+@section('content')
+    <h1>{{$conference['title']}}</h1>
+    <p>{{$conference['content']}}</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('Welcome');
-});
+Route::get('/', static function () {
+    return view('home.index', []);
+})->name('home.index');
+
+Route::get('/contact', static function () {
+    return view('home.contact', []);
+})->name('home.contact');
+
+Route::get('/conference/{id}', function ($conferenceId) {
+    $conferences = [
+        1=>[
+            'title' => 'First article',
+            'content' => 'First conference text 12345'
+        ],
+        2=>[
+            'title' => 'Second article',
+            'content' => 'Second conference text 12345'
+        ]
+    ];
+    return view('conferences.show', ['conference' => $conferences[$conferenceId]]);
+})->name('conference.show');
+
 
 //Route::get('/', function () {
 //    return 'Welcome to the home page';
